ac2fc6d041c9fcb623f10f9a1e144b1b9a2e86a4
Aritra Nanda <nandaaritra@gmail.com>
2025-02-08 12:19:03 +0530

    Added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
-export default function App() {
-  return (
-    <div className='test'>
-      <h1>Helloo</h1>
-    </div>
+export default function App(){
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+
+    </Routes>
+    </BrowserRouter>
   )
 }
