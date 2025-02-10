c4e32f9da5c06410281dc4af6c3cd28f588b8dd8
Aritra Nanda <nandaaritra@gmail.com>
2025-02-10 20:28:22 +0530

    Sign up page functionality complete

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SignIn.css"; // Import the CSS file
 
-export default function SignIn(){
-    return(
-        <h1>SignIn</h1>
-    )
+export default function SignIn() {
+  return (
+    <div className="signin-container">
+      <div className="signin-box">
+        <form className="signin-form">
+          <span className="title">Sign In</span>
+          <span className="subtitle">Welcome back! Please enter your details.</span>
+
+          <div className="form-container">
+            <input type="email" placeholder="Email" className="input" />
+            <input type="password" placeholder="Password" className="input" />
+          </div>
+
+          <button className="signin-button">Sign In</button>
+        </form>
+
+        <div className="form-section">
+          <p>
+            Don't have an account?{" "}
+            <Link to="/signup" className="signup-link">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./SignUp.css"; // Import the CSS file
 
-export default function SignUp(){
-    return(
-        <h1>SignUp</h1>
-    )
+export default function SignUp() {
+    const [formData, setFormData] = useState({})
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate=useNavigate();
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    };
+    
+    const handleSubmit = async(e) => {
+        e.preventDefault(); // Prevents page reload
+
+        try {
+            const res = await fetch('/api/auth/signup',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData),
+            } );
+            const data = await res.json();
+            if(data.success===false){
+                setLoading(false);
+                setError(data.message);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate('/signin');
+            
+        } catch (error) {
+            setLoading(false);
+            setError(error.message);
+        }
+        
+    };
+    
+    
+  return (
+    <div className="signup-container">
+      <div className="signup-box">
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <span className="title">Sign up</span>
+          <span className="subtitle">Create a free account with your email.</span>
+
+          <div className="form-container">
+            <input type="text" placeholder="Full Name" className="input" id="username" required onChange={handleChange}/>
+            <input type="email" placeholder="Email" className="input" id="email" required onChange={handleChange}/>
+            <input type="password" placeholder="Password" className="input" id="password" required onChange={handleChange}/>
+          </div>
+
+          <button disabled={loading} className="signup-button">{loading?'Loading...':'Sign Up'}</button>
+        </form>
+        {error && <p className="error">{error}</p>}
+
+        <div className="form-section">
+          <p>
+            Have an account?{" "}
+            <Link to="/signin" className="login-link">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
