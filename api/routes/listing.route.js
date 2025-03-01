0f877e9651fa84a8b972a2164aef474a45209e9c
Aritra Nanda <nandaaritra@gmail.com>
2025-03-01 19:05:31 +0530

    create listing api route

---
+++ api/routes/listing.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { createListing, } from '../controllers/listing.controller.js';
+import { verifyToken2 } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.post('/create', verifyToken2, createListing);
+// router.delete('/delete/:id', verifyToken2, deleteListing);
+// router.post('/update/:id', verifyToken2, updateListing);
+// router.get('/get/:id', getListing);
+// router.get('/get', getListings);
+
+export default router;
