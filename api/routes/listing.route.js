ef4c5164bc855874ee0e1ebfe26f3063466c62ce
Aritra Nanda <nandaaritra@gmail.com>
2025-03-07 19:22:46 +0530

    search properties api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 import { verifyToken2 } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,6 +8,6 @@ router.post('/create', verifyToken2, createListing);
 router.delete('/delete/:id', verifyToken2, deleteListing);
 router.post('/update/:id', verifyToken2, updateListing);
 router.get('/get/:id', getListing);
-// router.get('/get', getListings);
+router.get('/get', getListings);
 
 export default router;
